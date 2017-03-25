7540a684f13b469b1fec8761ea79035d0e22eefc
Robert Thomson <RTSketch@gmail.com>
2017-03-25 19:05:14 +0000

    Revert "Initial commit"
    This reverts commit 65d403b669c834db47108cc9bfed68c85f5d52a1.

--- routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$router->map('GET', '/', 'BetterBeing\Controllers\PageController@getShowHomePage', 'home');
-
-$router->map('GET', '/register', 'BetterBeing\Controllers\RegisterController@getShowRegisterPage', 'register');
-
-$router->map('POST', '/register', 'BetterBeing\Controllers\RegisterController@postShowRegisterPage', 'register_post');
--- src/models/User.php
+++
@@ -1,8 +0,0 @@
-<?php namespace BetterBeing\Models;
-use Illuminate\Database\Eloquent\Model as Eloquent;
-
-class User extends Eloquent {
-
-    public $timestamps = false;
-    
-}
